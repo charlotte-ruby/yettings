fa65cdb7b6df12a94beecbfebd3e63cce74b5dad
dima <pronix.service@gmail.com>
2012-03-30 21:53:40 +0200

    add ability change settings

--- lib/yettings.rb
+++ lib/yettings.rb
@@ -5,27 +5,34 @@ require "#{YETTINGS_PATH}/railtie.rb"
 
 module Yettings
   class UndefinedYetting < StandardError; end
+
   class << self
     def setup!
       find_ymls.each do |yml|
         create_yetting_class(yml)
       end
     end
-  
+
     def find_ymls
       main_file = "#{Rails.root.to_s}/config/yetting.yml"
       yettings_main_file = File.exists?(main_file) ? [main_file] : []
       yettings_namespaced_files = Dir.glob("#{Rails.root.to_s}/config/yettings/**/*.yml")
       yettings_main_file.concat(yettings_namespaced_files)
     end
-  
+
     def create_yetting_class(yml_file)
       hash = load_yml(yml_file)
       klass_name = File.basename(yml_file).gsub(".yml","").camelize
       klass_name = "#{klass_name}Yetting" unless klass_name=="Yetting"
       klass = Object.const_set(klass_name,Class.new)
       hash.each do |key,value|
-        klass.define_singleton_method(key){ value }
+        @@key = key
+        klass.class_eval do
+          class << self
+            attr_accessor @@key.to_sym
+          end
+        end
+        klass.instance_variable_set("@#{key}",value)
       end
       klass.class_eval do
         def self.method_missing(method_id,*args)
@@ -33,7 +40,7 @@ module Yettings
         end
       end
     end
-  
+
     def load_yml(yml_file)
       erb = ERB.new(File.read(yml_file)).result
       YAML.load(erb).to_hash[Rails.env]
